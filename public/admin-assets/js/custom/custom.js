7d6277d9dd266159239fdd41810c125db315da77
Asad Shahid <asadshahid245@gmail.com>
2023-03-15 14:06:42 -0400

    changes in custom js

--- public/admin-assets/js/custom/custom.js
+++ public/admin-assets/js/custom/custom.js
@@ -22,14 +22,17 @@
 //         }
 //     });
 // })
-// var loop_count = 1;
-// function add_more(add_div) {
-//     loop_count++;
-//     var html = ' <div class="card" id="product_attribute_' + loop_count + '"> <div class="card-body"><div class="row">';
-//     html += '<a name="" id="" class="btn btn-danger" onclick="div_remove(' + loop_count + ')" style="float:right;" href="#" role="button">Remove</a>';
-//     html += '</div></div></div> ';
-//     jQuery('#product_attribute_' + add_div).append(html);
-// }
+
+function add_more() {
+    // alert('hello worldx')
+    var html = ' <div class="card"> <div class="card-body"><div class="row">';
+    html +=' <div class="col-4 mb-3"><label for="example-text-input" class="col-sm-6 col-form-label">RetailPrice</label><div class="col-sm-12"><input class="form-control" type="text"placeholder="Enter retail price" id="example-text-input"name="retail_price">@error("retail_price")<div class="text-danger">{{ $message }}</div>@enderror</div></div>';
+    html+='<div class="col-4 mb-3"><label for="example-text-input" class="col-sm-6 col-form-label">Discount Price</label><div class="col-sm-12"><input class="form-control" type="text"placeholder="Enter store price(if any)" id="example-text-input"name="store_price">@error("store_price")<div class="text-danger">{{ $message }}</div>@enderror</div></div>';
+    html+='<div class="col-4 mb-3"><label for="example-text-input" class="col-sm-6 col-form-label">Stock</label><div class="col-sm-12"><input class="form-control" type="text"placeholder="Enter store price" id="example-text-input"name="store_price">@error("store_price")<div class="text-danger">{{ $message }}</div>@enderror</div></div>';
+    html += '<a name="" id="" class="btn btn-danger col-md-2 m-3" onclick="div_remove()" style="float:right;" href="#" role="button">Remove</a>';
+    html += '</div></div></div> ';
+    jQuery('#add_value').append(html);
+}
 
 // function div_remove(remove_id) {
 //     jQuery('#product_attribute_' + remove_id).remove();
